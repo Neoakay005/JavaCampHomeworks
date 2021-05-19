3f891355f9ed89f7c31f2655c470a58659da1ba9
akayh <akayh@DESKTOP-268FD6B>
2021-05-19 08:55:23 +0300

    deneme değişikliği

--- src/campHomework/Main.java
+++ src/campHomework/Main.java
@@ -15,7 +15,6 @@ public class Main {
 	public static void main(String[] args) {
 		// ----PLAYER MANAGEMENT----
 		PlayerManager playerManager = new PlayerManager(new PlayerCheckManager());
-		
 		Player playerHalil = new Player();
 		playerHalil.setId(1);
 		playerHalil.setFirstName("Halil Ýbrahim");
